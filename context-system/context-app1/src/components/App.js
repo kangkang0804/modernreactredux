77001c70eb903151f510c4aaeddf7b8c9b303eec
michaelikang <michaelikang94@gmail.com>
2020-06-13 11:07:04 -0400

    context app

---
+++ context-system/context-app1/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div className='ui container'>
+        <h4>let's practice the react context system</h4>
+      </div>
+    )
+  }
+}
+
+export default App
