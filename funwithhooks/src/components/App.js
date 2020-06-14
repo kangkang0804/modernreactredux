a3cabcebd4154fe8fa705f2e5b38d6dd91007261
michaelikang <michaelikang94@gmail.com>
2020-06-14 09:05:24 -0400

    funwithhooks setup

---
+++ funwithhooks/src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class App extends React.Component {
+  render () {
+    return <div className='ui container'> hey, this is my new app</div>
+  }
+}
+
+export default App
